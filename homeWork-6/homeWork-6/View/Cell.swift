4643004cb0b87bbed66e8f8f4d017f164285431e
ZhukovMA <45825715+ZhukovMA@users.noreply.github.com>
2019-10-28 21:20:13 +0300

    Update Cell.swift

--- homeWork-6/homeWork-6/View/Cell.swift
+++ homeWork-6/homeWork-6/View/Cell.swift
@@ -38,7 +38,6 @@ class Cell: UITableViewCell {
         let widthRatio  = targetSize.width  / size.width
         let heightRatio = targetSize.height / size.height
         
-        // Figure out what our orientation is, and use that to form the rectangle
         var newSize: CGSize
         if(widthRatio > heightRatio) {
             newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
@@ -46,10 +45,8 @@ class Cell: UITableViewCell {
             newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
         }
         
-        // This is the rect that we've calculated out and this is what is actually used below
         let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
         
-        // Actually do the resizing to the rect using the ImageContext stuff
         UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
         image.draw(in: rect)
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
